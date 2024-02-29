e72ce49c2de30ee0588737948e20affb8b45fa78
SmileExpression <15018135093@163.com>
2024-02-29 17:37:44 +0800

    Test

---
+++ A1/tai-e/src/test/resources/dataflow/livevar/HelloWorld.java
@@ -0,0 +1,12 @@
+class HelloWorld {
+  public static void main(String[] args) {
+    int x = 0;
+    int y = 0;
+    int z = 0;
+    x = 1;
+    y = 2;
+    z = 3;
+    System.out.println("Hello World!");
+    System.out.println(x + y + z);
+  }
+}
