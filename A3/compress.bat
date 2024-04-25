fe3f270f14a896be5474cef26a43815414e3f429
SmileExpression <15018135093@163.com>
2024-04-25 18:55:36 +0800

    Create compress.bat

---
+++ A3/compress.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal
+
+:: 检查并删除已存在的文件夹
+if exist test rd /s /q test
+
+:: 检查并删除已存在的zip文件
+if exist test.zip del /f test.zip
+
+:: 创建文件夹
+mkdir test
+
+:: 移动文件到文件夹
+copy .\tai-e\src\main\java\pascal\taie\analysis\dataflow\analysis\DeadCodeDetection.java test
+
+:: 切换到目标文件夹
+cd test
+
+:: 压缩文件夹
+powershell -command "Compress-Archive -Path .\* -DestinationPath ..\test.zip"
+
+:: 切换回原目录
+cd ..
+
+endlocal
