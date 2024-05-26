d8c0240066de6a47cbd57c70c10c0a37ec9e0267
SmileExpression <15018135093@163.com>
2024-05-26 12:05:59 +0800

    Add comments

--- A5/tai-e/src/main/java/pascal/taie/analysis/pta/ci/Solver.java
+++ A5/tai-e/src/main/java/pascal/taie/analysis/pta/ci/Solver.java
@@ -116,58 +116,80 @@ class Solver {
         //  via visitor pattern, then finish me
         @Override
         public Void visit(New stmt) {
-            VarPtr p =  pointerFlowGraph.getVarPtr(stmt.getLValue());
-            workList.addEntry(p,new PointsToSet(heapModel.getObj(stmt)));
-
+            // 处理New语句
+            // 获取左值的变量指针
+            VarPtr p = pointerFlowGraph.getVarPtr(stmt.getLValue());
+            // 获取对象的指针集合
+            PointsToSet pointsToSet = (PointsToSet) heapModel.getObj(stmt);
+            // 将左值的变量指针和对象的指针集合添加到工作列表中
+            workList.addEntry(p, pointsToSet);
             return StmtVisitor.super.visit(stmt);
         }
 
         @Override
         public Void visit(Copy stmt) {
+            // 处理Copy语句
+            // 获取左值的变量指针
             VarPtr p = pointerFlowGraph.getVarPtr(stmt.getLValue());
+            // 获取右值的变量指针
             VarPtr q = pointerFlowGraph.getVarPtr(stmt.getRValue());
-            addPFGEdge(q,p);
+            // 将右值的变量指针和左值的变量指针之间添加指针流图边
+            addPFGEdge(q, p);
             return StmtVisitor.super.visit(stmt);
         }
 
         @Override
         public Void visit(LoadField stmt) {
-            if(stmt.isStatic()){
+            // 处理LoadField语句
+            if (stmt.isStatic()) {
+                // 如果是静态字段的加载
+                // 解析字段引用并获取字段
                 JField field = stmt.getFieldRef().resolve();
-                //获取load的字段 y = T.f
+                // 获取静态字段的指针
                 StaticField f = pointerFlowGraph.getStaticField(field);
+                // 获取左值的变量指针
                 VarPtr y = pointerFlowGraph.getVarPtr(stmt.getLValue());
-                addPFGEdge(f,y);
-
+                // 将静态字段的指针和左值的变量指针之间添加指针流图边
+                addPFGEdge(f, y);
             }
-
             return StmtVisitor.super.visit(stmt);
         }
 
         @Override
         public Void visit(StoreField stmt) {
-            if(stmt.isStatic()){
-                // T.f = y
+            // 处理StoreField语句
+            if (stmt.isStatic()) {
+                // 如果是静态字段的存储
+                // 解析字段引用并获取字段
                 StaticField f = pointerFlowGraph.getStaticField(stmt.getFieldRef().resolve());
+                // 获取右值的变量指针
                 VarPtr y = pointerFlowGraph.getVarPtr(stmt.getRValue());
-                addPFGEdge(y,f);
+                // 将右值的变量指针和静态字段的指针之间添加指针流图边
+                addPFGEdge(y, f);
             }
             return StmtVisitor.super.visit(stmt);
         }
 
         @Override
         public Void visit(Invoke stmt) {
-            if(stmt.isStatic()){
-                JMethod method = resolveCallee(null,stmt);
-
-                Edge<Invoke,JMethod> edge = new Edge<>(CallKind.STATIC, stmt,method);
-                if(callGraph.addEdge(edge)){
+            // 处理Invoke语句
+            if (stmt.isStatic()) {
+                // 如果是静态调用
+                // 解析目标方法
+                JMethod method = resolveCallee(null, stmt);
+                // 创建Invoke边，并将其添加到调用图中
+                Edge<Invoke, JMethod> edge = new Edge<>(CallKind.STATIC, stmt, method);
+                if (callGraph.addEdge(edge)) {
+                    // 如果成功添加边，则执行以下操作
+                    // 添加目标方法到可达方法的调用图中
                     addReachable(method);
-                    for(int i=0;i<method.getParamCount();i++){
-                        addPFGEdge(pointerFlowGraph.getVarPtr(stmt.getRValue().getArg(i)),pointerFlowGraph.getVarPtr(method.getIR().getParam(i)));
+                    // 处理参数之间的指针流图边
+                    for (int i = 0; i < method.getParamCount(); i++) {
+                        addPFGEdge(pointerFlowGraph.getVarPtr(stmt.getRValue().getArg(i)), pointerFlowGraph.getVarPtr(method.getIR().getParam(i)));
                     }
+                    // 处理返回值之间的指针流图边
                     for (Var returnVar : method.getIR().getReturnVars()) {
-                        addPFGEdge(pointerFlowGraph.getVarPtr(returnVar),pointerFlowGraph.getVarPtr(stmt.getLValue()));
+                        addPFGEdge(pointerFlowGraph.getVarPtr(returnVar), pointerFlowGraph.getVarPtr(stmt.getLValue()));
                     }
                 }
             }
@@ -180,11 +202,15 @@ class Solver {
      */
     private void addPFGEdge(Pointer source, Pointer target) {
         // TODO - finish me
-        if(!pointerFlowGraph.addEdge(source,target)){
+        if (!pointerFlowGraph.addEdge(source, target)) {
+            // 如果指针流图中已经存在该边，则不执行任何操作，直接返回。
             return;
         }
-        if(!source.getPointsToSet().isEmpty()){
-            workList.addEntry(target,source.getPointsToSet());
+
+        if (!source.getPointsToSet().isEmpty()) {
+            // 如果源指针的指向集合不为空
+            // 将目标指针和源指针的指向集合添加到工作列表中
+            workList.addEntry(target, source.getPointsToSet());
         }
     }
 
@@ -193,26 +219,41 @@ class Solver {
      */
     private void analyze() {
         // TODO - finish me
-        while(!workList.isEmpty()){
+        while (!workList.isEmpty()) {
+            // 只要工作列表不为空，就进行循环处理
+
             WorkList.Entry entry = workList.pollEntry();
+            // 从工作列表中获取一个条目
 
-            PointsToSet delta = propagate(entry.pointer(),entry.pointsToSet());
-            //propage返回delta
+            PointsToSet delta = propagate(entry.pointer(), entry.pointsToSet());
+            // 根据当前条目的指针和指向集合进行传播操作，并获取传播结果（delta）
 
-            if(entry.pointer() instanceof VarPtr){
-                Var var =  ((VarPtr) entry.pointer()).getVar();
-                for(Obj obj : delta){
-                    List<LoadArray> loadarrays   = var.getLoadArrays();
+            if (entry.pointer() instanceof VarPtr) {
+                Var var = ((VarPtr) entry.pointer()).getVar();
+                // 获取变量指针对应的变量对象
+
+                for (Obj obj : delta) {
+                    // 遍历传播结果中的每个对象
+
+                    List<LoadArray> loadarrays = var.getLoadArrays();
                     List<StoreArray> storearrays = var.getStoreArrays();
-                    List<LoadField>  loadfields  = var.getLoadFields();
-                    List<StoreField>  storefileds =var.getStoreFields();
+                    List<LoadField> loadfields = var.getLoadFields();
+                    List<StoreField> storefileds = var.getStoreFields();
 
-                    loadarrays.forEach(stmt  -> addPFGEdge(pointerFlowGraph.getArrayIndex(obj),pointerFlowGraph.getVarPtr(stmt.getLValue())));
-                    storearrays.forEach(stmt -> addPFGEdge(pointerFlowGraph.getVarPtr(stmt.getRValue()),pointerFlowGraph.getArrayIndex(obj)));
-                    loadfields.forEach(stmt  -> addPFGEdge(pointerFlowGraph.getInstanceField(obj,stmt.getFieldAccess().getFieldRef().resolve()),pointerFlowGraph.getVarPtr(stmt.getLValue())));
-                    storefileds.forEach(stmt -> addPFGEdge(pointerFlowGraph.getVarPtr(stmt.getRValue()),pointerFlowGraph.getInstanceField(obj,stmt.getFieldAccess().getFieldRef().resolve())));
+                    loadarrays.forEach(stmt -> addPFGEdge(pointerFlowGraph.getArrayIndex(obj), pointerFlowGraph.getVarPtr(stmt.getLValue())));
+                    // 对于变量的每个LoadArray语句，将数组索引指针和左值变量指针之间添加指针流图边
 
-                    processCall(var,obj);
+                    storearrays.forEach(stmt -> addPFGEdge(pointerFlowGraph.getVarPtr(stmt.getRValue()), pointerFlowGraph.getArrayIndex(obj)));
+                    // 对于变量的每个StoreArray语句，将右值变量指针和数组索引指针之间添加指针流图边
+
+                    loadfields.forEach(stmt -> addPFGEdge(pointerFlowGraph.getInstanceField(obj, stmt.getFieldAccess().getFieldRef().resolve()), pointerFlowGraph.getVarPtr(stmt.getLValue())));
+                    // 对于变量的每个LoadField语句，将实例字段指针和左值变量指针之间添加指针流图边
+
+                    storefileds.forEach(stmt -> addPFGEdge(pointerFlowGraph.getVarPtr(stmt.getRValue()), pointerFlowGraph.getInstanceField(obj, stmt.getFieldAccess().getFieldRef().resolve())));
+                    // 对于变量的每个StoreField语句，将右值变量指针和实例字段指针之间添加指针流图边
+
+                    processCall(var, obj);
+                    // 处理变量和对象之间的调用关系
                 }
             }
         }
@@ -225,16 +266,29 @@ class Solver {
     private PointsToSet propagate(Pointer pointer, PointsToSet pointsToSet) {
         // TODO - finish me
         PointsToSet delta = new PointsToSet();
-        pointsToSet.forEach(p ->{
-            if(pointer.getPointsToSet().addObject(p)) delta.addObject(p);
+        // 创建一个空的传播结果（delta）
+
+        pointsToSet.forEach(p -> {
+            // 遍历指向集合中的每个对象指针
+
+            if (pointer.getPointsToSet().addObject(p)) {
+                // 如果将对象指针添加到当前指针的指向集合中成功
+                // 将对象指针添加到传播结果（delta）中
+                delta.addObject(p);
+            }
         });
-        if(!delta.isEmpty()){
-            pointerFlowGraph.getSuccsOf(pointer).forEach(succ->{
-                workList.addEntry(succ,delta);
+
+        if (!delta.isEmpty()) {
+            // 如果传播结果（delta）不为空
+            // 遍历当前指针的后继节点
+            pointerFlowGraph.getSuccsOf(pointer).forEach(succ -> {
+                // 将后继节点和传播结果（delta）添加到工作列表中
+                workList.addEntry(succ, delta);
             });
         }
 
         return delta;
+        // 返回传播的结果（delta）
     }
 
     /**
@@ -245,25 +299,37 @@ class Solver {
      */
     private void processCall(Var var, Obj recv) {
         // TODO - finish me
-        var.getInvokes().forEach(invoke->{
-            JMethod method = resolveCallee(recv,invoke);
-            workList.addEntry(pointerFlowGraph.getVarPtr(method.getIR().getThis()),new PointsToSet(recv));
+        var.getInvokes().forEach(invoke -> {
+            // 遍历变量的每个调用语句
+
+            JMethod method = resolveCallee(recv, invoke);
+            // 解析调用目标方法
+
+            workList.addEntry(pointerFlowGraph.getVarPtr(method.getIR().getThis()), new PointsToSet(recv));
+            // 将目标方法的this指针和对象的指向集合添加到工作列表中
 
             CallKind callkind = null;
-            if(invoke.isStatic()) callkind = CallKind.STATIC;
-            if(invoke.isInterface()) callkind = CallKind.INTERFACE;
-            if(invoke.isSpecial()) callkind = CallKind.SPECIAL;
-            if(invoke.isVirtual()) callkind = CallKind.VIRTUAL;
+            if (invoke.isStatic()) callkind = CallKind.STATIC;
+            if (invoke.isInterface()) callkind = CallKind.INTERFACE;
+            if (invoke.isSpecial()) callkind = CallKind.SPECIAL;
+            if (invoke.isVirtual()) callkind = CallKind.VIRTUAL;
 
-            Edge<Invoke,JMethod> edge = new Edge<>(callkind,invoke,method);
-            if(callGraph.addEdge(edge)){
+            Edge<Invoke, JMethod> edge = new Edge<>(callkind, invoke, method);
+            if (callGraph.addEdge(edge)) {
+                // 如果成功添加调用图的边，则执行以下操作
+
                 addReachable(method);
-                for(int i=0;i<method.getParamCount();i++){
-                    addPFGEdge(pointerFlowGraph.getVarPtr(invoke.getRValue().getArg(i)),pointerFlowGraph.getVarPtr(method.getIR().getParam(i)));
+                // 将目标方法添加到可达方法的调用图中
+
+                for (int i = 0; i < method.getParamCount(); i++) {
+                    addPFGEdge(pointerFlowGraph.getVarPtr(invoke.getRValue().getArg(i)), pointerFlowGraph.getVarPtr(method.getIR().getParam(i)));
+                    // 处理参数之间的指针流图边
                 }
-                if(invoke.getLValue()!=null){
+
+                if (invoke.getLValue() != null) {
                     for (Var returnVar : method.getIR().getReturnVars()) {
-                        addPFGEdge(pointerFlowGraph.getVarPtr(returnVar),pointerFlowGraph.getVarPtr(invoke.getLValue()));
+                        addPFGEdge(pointerFlowGraph.getVarPtr(returnVar), pointerFlowGraph.getVarPtr(invoke.getLValue()));
+                        // 处理返回值之间的指针流图边
                     }
                 }
             }
